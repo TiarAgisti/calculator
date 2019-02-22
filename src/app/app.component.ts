49719eaa221a37dc3b894834f17430aecfea6994
Agieaja <tiar@edutren.com>
2019-02-22 08:56:18 +0700

    init first project with angular 7

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { summaryFileName } from '@angular/compiler/src/aot/util';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'calculator';
+
+  sum: number;
+  number1: number;
+  number2: number;
+  selOperation: string;
+  pesan: string;
+
+
+  Calculate(){
+    if (!this.number1 || !this.number2){
+      this.pesan ="Angka tidak boleh kosong";
+      return
+    }
+
+    if(!this.selOperation){
+      this.pesan ="Operasi belum dipilih";
+      return
+    }
+
+    if (this.selOperation == "+"){
+      this.sum=this.number1 + this.number2;
+    }
+
+    if (this.selOperation == "-"){
+      this.sum=this.number1 - this.number2;
+    }
+
+    if (this.selOperation == "/"){
+      this.sum=this.number1 / this.number2;
+    }
+
+    if (this.selOperation == "x"){
+      this.sum=this.number1 * this.number2;
+    }
+
+    this.pesan="Hasilnya adalah " + this.sum;
+  }
 }
